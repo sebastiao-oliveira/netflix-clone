b4bf2da38d81461efb097239f6dd21b902e3bd1a
SEBASTIAO DE OLIVEIRA MELO <54176273+sebastiao-oliveira@users.noreply.github.com>
2022-01-29 14:51:57 -0300

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "9dad9031534f129dac21ae18cc6b7039";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";   
